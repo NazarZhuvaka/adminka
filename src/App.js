cb522cd8e102a9318b6ccc2ed78ff12d5312504a
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-10-02 18:02:20 +0200

    Add store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import { store } from './store/store'
 import HomePage from './pages/HomePage'
-import SignInForm from './components/SignInForm';
-import SignUpForm from './components/SignUpForm';
-import NotFoundPage from './pages/NotFoundPage';
-import AdminPage from './pages/AdminPage';
+import SignInForm from './components/SignInForm'
+import SignUpForm from './components/SignUpForm'
+import NotFoundPage from './pages/NotFoundPage'
+import AdminPage from './pages/AdminPage'
 
 function App () {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' Component={HomePage}/>
-        <Route path='/signIn' Component={SignInForm}/>
-        <Route path='/signUp' Component={SignUpForm}/>
-        <Route path='/adminPage' Component={AdminPage}/>
-        <Route path='/*' Component={NotFoundPage}/>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path='/' Component={HomePage} />
+          <Route path='/signIn' Component={SignInForm} />
+          <Route path='/signUp' Component={SignUpForm} />
+          <Route path='/adminPage' Component={AdminPage} />
+          <Route path='/*' Component={NotFoundPage} />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
--- src/schemas.js
+++ src/schemas.js
@@ -3,11 +3,11 @@ import * as yup from 'yup'
 export const SIGN_UP_SCHEMA = yup.object({
   firstName: yup.string().required().min(1).max(50),
   lastName: yup.string().required().min(1).max(50),
-  email:  yup.string().email().required(),
+  email: yup.string().email().required(),
   password: yup.string().min(5).max(50).matches()
 })
 
 export const SIGN_IN_SCHEMA = yup.object({
   password: yup.string().min(5).max(50).matches(),
-  email:  yup.string().email().required(),
+  email: yup.string().email().required()
 })
